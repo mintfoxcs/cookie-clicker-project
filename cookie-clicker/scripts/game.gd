d0145e6a3d918f8e3c6f858ee5b38b2d3fa0934f
Daniel Price <danielprice606@gmail.com>
2026-08-17 17:45:37 +0100

    Add files via upload

--- cookie-clicker/scripts/game.gd
+++ cookie-clicker/scripts/game.gd
@@ -1,10 +1,35 @@
 extends Control
 
+const save_path = "user://userdata.save"
+
 var cookies = 0
 var amount_per_click = 1
 
 signal cookies_changed
 
+func _ready():
+	load_data()
+	emit_signal("cookies_changed", cookies)
+
+func save_data():
+	var data = {
+		"cookies": cookies,
+	}
+	var file = FileAccess.open(save_path, FileAccess.WRITE)
+	file.store_var(data)
+	file.close()
+	
+func load_data():
+	if FileAccess.file_exists(save_path):
+		var file = FileAccess.open(save_path, FileAccess.READ)
+		var data = file.get_var()
+		file.close()
+		if typeof(data) == TYPE_DICTIONARY:
+			cookies = data.get("cookies", 0)
+	else:
+		save_data()	
+
 func _on_cookie_button_down() -> void:
 	cookies += amount_per_click
 	emit_signal("cookies_changed", cookies)
+	save_data()
